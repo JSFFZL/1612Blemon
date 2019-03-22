9d673642a12897fc6eb2f536f752ddc3dee1847d
mqf <452926729@qq.com>
2019-03-22 14:00:30 +0800

    封装接口

--- lemon/api/routes/index.js
+++ lemon/api/routes/index.js
@@ -1,14 +1,59 @@
 var express = require('express');
 var router = express.Router();
-var Mongo = require("")
+var Mongo = require('Mongodb-curd');
+
+var db = "1612Blemon";
+var coll_user = "user";//
+var coll_bill = "bill";
+var coll_icon = "icon";//
 
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
 
-
-
-
+//查询账单
+router.post('/api/getBill', function(req, res, next) {
+  var uid = req.body.uid;
+  Mongo.find(db,coll_bill,{"uid":uid},function(result){
+    if(!result){
+      res.json({code:0,msg:"查询失败"})
+    }else{
+      res.json({code:1,data:result})
+    }
+  })
 });
+
+
+
+//删除账单
+router.post('/api/deleteBill', function(req, res, next) {
+  var id = req.body.id;
+  Mongo.remove(db,coll_bill,{"_id":id},function(result){
+    if(!result){
+      res.json({code:0,msg:"删除失败"})
+    }else{
+      res.json({code:1,msg:"删除成功"})
+    }
+  })
+});
+
+//添加账单
+router.post('/api/insertBill', function(req, res, next) {
+  var obj = req.body;
+  Mongo.insert(db,coll_bill,obj,function(result){
+    if(!result){
+      res.json({code:0,msg:"添加账单失败"})
+    }else{
+      res.json({code:1,msg:"添加账单成功"})
+    }
+  })
+});
+
+
+
+
+
+
+
+
 
 module.exports = router;
